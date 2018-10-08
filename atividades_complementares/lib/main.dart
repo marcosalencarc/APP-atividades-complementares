f5cdd59ce86ba606e71962244e48ee110c4712f0
Marcos de Alencar Carvalho <marcoscarvalho1997@gmail.com>
2018-10-08 13:30:16 -0300

    Implementação de circular progress

--- atividades_complementares/lib/main.dart
+++ atividades_complementares/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:atividades_complementares/ui/cadastro_user_page.dart';
+import 'package:atividades_complementares/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
 
   runApp(MaterialApp(
-    home: CadastroUserPage(),
+    home: HomePage(),
     debugShowCheckedModeBanner: false,
   ));
 }
